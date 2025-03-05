74461953de9699f19dd0b668f1712954bb3ab3ca
YamiHeike <koscielniak.z@gmail.com>
2025-03-05 22:58:43 +0100

    added GenreRepository

---
+++ src/main/java/com/example/shelfofshame/book/genre/GenreRepository.java
@@ -0,0 +1,9 @@
+package com.example.shelfofshame.book.genre;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GenreRepository extends JpaRepository<Genre, Long> {
+    Genre findByName(String name);
+}
