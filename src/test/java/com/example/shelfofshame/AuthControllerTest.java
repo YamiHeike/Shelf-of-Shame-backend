cf03092b0535edb49572d84f47566e46e16cbb9f
YamiHeike <koscielniak.z@gmail.com>
2025-03-25 22:08:49 +0100

    test: login

---
+++ src/test/java/com/example/shelfofshame/AuthControllerTest.java
@@ -0,0 +1,55 @@
+package com.example.shelfofshame;
+
+import com.example.shelfofshame.user.UserService;
+import com.example.shelfofshame.user.dto.CredentialsDto;
+import com.example.shelfofshame.user.dto.UserDto;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import static org.hamcrest.Matchers.matchesPattern;
+import static org.mockito.ArgumentMatchers.matches;
+import static org.mockito.Mockito.doReturn;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@AutoConfigureMockMvc
+public class AuthControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockitoBean
+    private UserService userService;
+
+    @Test
+    protected void testLogin() throws Exception {
+        CredentialsDto credentialsDto = CredentialsDto.builder()
+                .email("test@example.com")
+                .password("password123")
+                .build();
+        UserDto mockUser = UserDto.builder()
+                .id(1L)
+                .email("test@example.com")
+                .username("test")
+                .token("test")
+                .build();
+       doReturn(mockUser).when(userService).login(credentialsDto);
+        mockMvc.perform(post("/login")  // Set contentType on request, not response
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"email\": \"test@example.com\", \"password\": \"password123\"}"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.email").value("test@example.com"))
+                .andExpect(jsonPath("$.username").value("test"))
+                .andExpect(jsonPath("$.token", matchesPattern("^[\\w-]*\\.[\\w-]*\\.[\\w-]*$")));
+    }
+}
