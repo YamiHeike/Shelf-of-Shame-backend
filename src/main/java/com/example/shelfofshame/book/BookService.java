3d74400f51bb0c131a52f64b91c7c6fc132c98da
YamiHeike <koscielniak.z@gmail.com>
2025-04-06 19:09:33 +0200

    added  book repository

---
+++ src/main/java/com/example/shelfofshame/book/BookService.java
@@ -0,0 +1,25 @@
+package com.example.shelfofshame.book;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BookService {
+    private final BookRepository bookRepository;
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Book getByIsbn(String isbn) {
+        return bookRepository.findByIsbn(isbn).orElse(null);
+    }
+
+    public Book getByTitle(String title) {
+        return bookRepository.findByTitle(title).orElse(null);
+    }
+
+}
