14dd9f48d12547270324a6e9cd8f739d8d75cf26
YamiHeike <koscielniak.z@gmail.com>
2025-07-19 22:55:27 +0200

    swagger setup

---
+++ src/main/java/com/example/shelfofshame/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.example.shelfofshame.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Shelf of Shame")
+                        .version("1.0")
+                        .description("This REST API powers the Shelf of Shame app, allowing users to manage personal reading lists, track progress, and organize books by genre and difficulty. Created as a portfolio project.")
+                );
+    }
+}
