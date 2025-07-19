045cd02f9eb9ea0077ae61357da3c5dda3c08a4b
YamiHeike <koscielniak.z@gmail.com>
2025-07-19 22:34:46 +0200

    bugfix: difficulty validation

--- src/main/java/com/example/shelfofshame/user/shelf/dto/AddBookToShelfDto.java
+++ src/main/java/com/example/shelfofshame/user/shelf/dto/AddBookToShelfDto.java
@@ -4,9 +4,7 @@ package com.example.shelfofshame.user.shelf.dto;
 import com.example.shelfofshame.user.shelf.Status;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,7 +17,8 @@ public class AddBookToShelfDto {
     @NotBlank
     private String isbn;
     private String notes;
-    @Size(min = 1, max = 10)
+    @Min(value = 1, message = "difficulty scale: 1-10")
+    @Max(value = 10, message = "difficulty scale: 1-10")
     private int difficulty;
     @Enumerated(EnumType.STRING)
     private Status status;
